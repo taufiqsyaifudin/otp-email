6bb63e25d035dd1c44148a8ae41c9829e664900f
taufiqsyaifudin <taufiqsyaifudin.dev@gmail.com>
2023-01-17 09:07:56 +0700

    otp-email

--- signup-user.php
+++ signup-user.php
@@ -2,67 +2,76 @@
 <?php require_once "header.php"; ?>
 
 <div class="container">
-    <div class="row">
-        <div class="col-sm-4">
-            <!-- Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  <div class="row">
+    <div class="col-sm-4">
+      <!-- Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
             proident, sunt in culpa qui officia deserunt mollit anim id est laborum. -->
-        </div>
-        <div class="col-sm-4">
-            <form action="signup-user.php" method="POST" autocomplete="" class="form-horizontal formx" role="form">
-                <h2 class="text-center">Form Registrasi</h2>
-                <p class="text-center">Silahkan lengkapi form di bawah ini!</p>
-                <?php
-                if (count($errors) == 1) {
-                ?>
-                    <div class="alert alert-danger text-center">
-                        <?php
-                        foreach ($errors as $showerror) {
-                            echo $showerror;
-                        }
-                        ?>
-                    </div>
-                <?php
-                } elseif (count($errors) > 1) {
-                ?>
-                    <div class="alert alert-danger">
-                        <?php
-                        foreach ($errors as $showerror) {
-                        ?>
-                            <li><?php echo $showerror; ?></li>
-                        <?php
-                        }
-                        ?>
-                    </div>
-                <?php
-                }
-                ?>
-                <div class="form-group">
-                    <input class="form-control" type="text" name="name" placeholder="Nama Lengkap" required value="<?php echo $name ?>">
-                </div>
-                <div class="form-group">
-                    <input class="form-control" type="email" name="email" placeholder="Alamat Email Aktif" required value="<?php echo $email ?>">
-                </div>
-                <div class="form-group">
-                    <input class="form-control" type="password" name="password" placeholder="Password" required>
-                </div>
-                <div class="form-group">
-                    <input class="form-control" type="password" name="cpassword" placeholder="Konfirmasi password" required>
-                </div>
-                <div class="form-group">
-                    <Center>
-                        <button class="btn btn-sm btn-primary" type="submit" name="signup" style="width: 150px;border-radius: 20px;border: 2px solid #EFE4E4;"><i class="fa fa-user-plus faa-tada animated-hover"></i> Registrasi</button>
-                        <a href="login" class="btn btn-sm btn-success" style="width: 150px;border-radius: 20px;border: 2px solid #EFE4E4;"><i class="fa fa-close faa-tada animated-hover"></i> Batal</a>
-                    </Center>
-                </div>
-                <div class="link login-link text-center">Sudah memiliki akun?
-                    <a href="login" class="label btn-warning" title="Klik Saya">Login Sekarang!</a>
-                </div>
-            </form>
-        </div>
     </div>
+    <div class="col-sm-4">
+      <form action="signup-user.php" method="POST" autocomplete="" class="form-horizontal formx" role="form">
+        <h2 class="text-center">Form Registrasi</h2>
+        <p class="text-center">Silahkan lengkapi form di bawah ini!</p>
+        <?php
+        if (count($errors) == 1) {
+        ?>
+          <div class="alert alert-danger text-center">
+            <?php
+            foreach ($errors as $showerror) {
+              echo $showerror;
+            }
+            ?>
+          </div>
+        <?php
+        } elseif (count($errors) > 1) {
+        ?>
+          <div class="alert alert-danger">
+            <?php
+            foreach ($errors as $showerror) {
+            ?>
+              <li><?php echo $showerror; ?></li>
+            <?php
+            }
+            ?>
+          </div>
+        <?php
+        }
+        ?>
+        <div class="form-group">
+          <input class="form-control" type="text" name="name" placeholder="Nama Lengkap" required value="<?php echo $name ?>">
+        </div>
+        <div class="form-group">
+          <input class="form-control" type="email" name="email" placeholder="Alamat Email Aktif" required value="<?php echo $email ?>">
+        </div>
+        <div class="form-group">
+          <input class="form-control" type="password" name="password" placeholder="Password" required>
+        </div>
+        <div class="form-group">
+          <input class="form-control" type="password" name="cpassword" placeholder="Konfirmasi password" required>
+        </div>
+        <div class="form-group">
+          <Center>
+            <button class="btn btn-sm btn-primary" type="submit" name="signup" style="width: 150px;border-radius: 20px;border: 2px solid #EFE4E4;"><i class="fa fa-user-plus faa-tada animated-hover"></i> Registrasi</button>
+            <a href="login" class="btn btn-sm btn-success" style="width: 150px;border-radius: 20px;border: 2px solid #EFE4E4;"><i class="fa fa-close faa-tada animated-hover"></i> Batal</a>
+          </Center>
+        </div>
+        <div class="link login-link text-center">Sudah memiliki akun?
+          <a href="login" class="label btn-warning" title="Klik Saya">Login Sekarang!</a>
+        </div>
+      </form>
+    </div>
+
+    <div class="col-md-4">
+      <!-- Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. -->
+    </div>
+  </div>
 </div>
 <?php require_once "footer.php"; ?>
